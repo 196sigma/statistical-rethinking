71a2a2c71e7d51489451187f6ea3b8c2420ba671
Reginald Edwards <reginald.edwards@gmail.com>
2020-06-01 18:46:15 -0500

    more chapter 3 problems

--- chapter03/chapter03-problems.R
+++ chapter03/chapter03-problems.R
@@ -3,6 +3,8 @@ gc()
 
 library(ggplot2)
 library(rethinking)
+
+
 p_grid <- seq(from=0, to=1, length.out=1000)
 prior <- rep(1, 1000)
 likelihood <- dbinom(6, size=9, prob=p_grid)
@@ -36,8 +38,16 @@ prior <- rep(1, 1000)
 likelihood <- dbinom(8, size=15, prob=p_grid)
 posterior <- likelihood*prior
 posterior <- posterior/sum(posterior)
-samples <- sample(p_grid, prob=posterior, size=1e4, replace=TRUE)
-ggplot(NULL, aes(x=samples)) + geom_density()
+post.df <- data.frame(posterior=posterior, ind=seq(1, length(posterior)))
+ggplot() + geom_line(aes(y=posterior, x=ind), data = post.df)
+
+samples <- sample(p_grid, size=1e4, replace=TRUE, prob=posterior)
+
+w <- rbinom(1e4, size=15, prob=samples)
+plot(density(w))
+rethinking::simplehist(w)
+
+length(w[w == 8])/length(w)
 
 
 birth1 <- c(1,0,0,0,1,1,0,1,0,1,0,0,1,1,0,1,1,0,0,0,1,0,0,0,1,0,
